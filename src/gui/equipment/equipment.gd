1afe5267323048a159e6968078ba8f8c2f681111
tuananhcn <nguyenanhbee12@gmail.com>
2024-10-09 11:36:41 +0700

    update equipment gui

--- src/gui/equipment/equipment.gd
+++ src/gui/equipment/equipment.gd
@@ -1,11 +1,16 @@
 extends CanvasLayer
 
-
+@onready var weapon_slot = $NinePatchRect2/GridContainer/WeaponSlot
+@onready var armor_slot = $NinePatchRect2/GridContainer/ArmorSlot
+@onready var head_slot = $NinePatchRect2/GridContainer/HeadSlot
+@onready var accessory_slot = $NinePatchRect2/GridContainer/AccessorySlot
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	setup_equipment_slots()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func setup_equipment_slots():
+	# Assuming each NinePatchRect has a TextureRect inside for the icon
+	weapon_slot.get_node("TextureRect").texture = load("res://textures/weapon_icon.png")
+	armor_slot.get_node("TextureRect").texture = load("res://textures/armor_icon.png")
+	head_slot.get_node("TextureRect").texture = load("res://textures/head_icon.png")
+	accessory_slot.get_node("TextureRect").texture = load("res://textures/accessory_icon.png")
