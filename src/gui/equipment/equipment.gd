05a67312900a6c46c87caa7cadc96b9b3b6b0f90
tuananhcn <nguyenanhbee12@gmail.com>
2024-10-10 05:49:55 +0700

    Fix blur in gui equipment

--- src/gui/equipment/equipment.gd
+++ src/gui/equipment/equipment.gd
@@ -6,11 +6,4 @@ extends CanvasLayer
 @onready var accessory_slot = $NinePatchRect2/GridContainer/AccessorySlot
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	setup_equipment_slots()
-
-func setup_equipment_slots():
-	# Assuming each NinePatchRect has a TextureRect inside for the icon
-	weapon_slot.get_node("TextureRect").texture = load("res://textures/weapon_icon.png")
-	armor_slot.get_node("TextureRect").texture = load("res://textures/armor_icon.png")
-	head_slot.get_node("TextureRect").texture = load("res://textures/head_icon.png")
-	accessory_slot.get_node("TextureRect").texture = load("res://textures/accessory_icon.png")
+	pass
